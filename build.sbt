c8360b52f90b881f2955d5a3b8f62043122a2da7
Tyler Horth <tylerhorth@outlook.com>
2018-10-04 15:41:32 -0700

    Cross compile ScalaDocsProvider
    Created seperate project for the ScalaDocsProvider:
    - ScalaDocsProvider needs access to the java classpath
    - Sbt by default runs tests in the same JVM process as Sbt, which mangles
      the classpath
    - Enabling test forking resolves the classpath issue, however breaks the test
      runner when cross-building between sbt versions.
    - Using a seperate project we can cross-build between scala versions and add
      it as a dependency from the main plugin project

--- build.sbt
+++ build.sbt
@@ -1,17 +1,30 @@
-enablePlugins(SbtPlugin)
+val pegasusVersion = "24.0.2"
 
-name := "sbt-restli"
+val root = (project in file("."))
+  .enablePlugins(SbtPlugin)
+  .dependsOn(restliToolsScala)
+  .settings(
+    name := "sbt-restli",
+    version := "0.3.0",
+    organization := "com.linkedin.pegasus",
+    crossSbtVersions := Seq("1.2.3", "0.13.17"),
+    libraryDependencies ++= Seq(
+    )
+  )
 
-version := "0.3.0"
-
-organization := "com.linkedin.pegasus"
-
-crossSbtVersions := Seq("1.2.3")
-
-val pegasusVersion = "23.0.+"
-
-libraryDependencies ++= Seq(
-  "com.linkedin.pegasus" % "restli-server" % pegasusVersion,
-  "com.linkedin.pegasus" % "restli-int-test-server" % pegasusVersion % Test,
-  "org.testng" % "testng" % "6.14.3" % Test,
-)
+lazy val restliToolsScala = (project in file("restli-tools-scala"))
+  .settings(
+    name := "restli-tools-scala",
+    version := "24.0.2",
+    organization := "com.linkedin.pegasus",
+    crossScalaVersions := Seq("2.10.7", "2.12.6"),
+    // Do not remove this line or tests break. Sbt mangles the java.class.path system property unless forking is enabled :(
+    fork in Test := true,
+    libraryDependencies ++= Seq(
+      "org.specs2" %% "specs2-core" % "3.9.4" % Test,
+      "org.specs2" %% "specs2-matcher-extra" % "3.9.4" % Test,
+      "com.linkedin.pegasus" % "restli-int-test-api" % pegasusVersion % Test classifier "all",
+      "com.linkedin.pegasus" % "restli-server" % pegasusVersion,
+      "org.scala-lang" % "scala-compiler" % scalaVersion.value,
+    )
+  )
---
+++ restli-tools-scala/src/test/scala/com/linkedin/restli/tools/scala/TestScalaDocsProvider.scala
@@ -0,0 +1,78 @@
+/*
+   Copyright (c) 2014 LinkedIn Corp.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package com.linkedin.restli.tools.scala
+
+import java.nio.file.Paths
+import java.util.{Collections => JavaCollections}
+
+import org.specs2.matcher.FileMatchers
+import org.specs2.mutable._
+
+class TestScalaDocsProvider extends Specification with FileMatchers {
+  override def is = s2"""
+    The ScalaGreetingsResource should
+      exist                             $t1
+
+    The ScalaDocsProvider should generate docs for
+      a resource class                  $t2
+      a resource method                 $t3
+      a resource method parameter       $t4
+      an action                         $t5
+      an actions first parameter        $t6
+      an actions second parameter       $t7
+    """
+
+  private val resource = "src/test/scala/com/linkedin/restli/tools/scala/ScalaGreetingsResource.scala"
+
+  private def t1 = resource must beAnExistingPath
+
+  private val files = JavaCollections.singleton(Paths.get(resource).toAbsolutePath.toString)
+
+  private val provider = new ScalaDocsProvider(null)
+  provider.registerSourceFiles(files)
+
+  private val method = classOf[ScalaGreetingsResource].getMethod("get", classOf[java.lang.Long])
+
+  // behavior appears to have regressed in 2.10.  The code in the below <pre> tag no longer is tabbed properly like it was in 2.9.  Lame.
+  // ^ Fixed in scala 2.12. Since we still support 2.10 must test both variations.
+  private def t2 = provider.getClassDoc(classOf[ScalaGreetingsResource]) mustEqual TestCompat.resourceTestString
+
+
+  private def t3 = compareDocString(
+    """<p>Now let's test some html formatted scaladoc.</p>
+      |<p><b>Some html</b> with a <a href="http://rest.li">link</a>. x<sup>a</sup><sub>b</sub>.</p>
+      |<ul>
+      |<li><p>unordered bullet 1</p></li>
+      |<li><p>unordered bullet 2</p></li>
+      |</ul>""".stripMargin,
+    provider.getMethodDoc(method))
+
+  private def t4 = compareDocString("<p>provides the key.</p>", provider.getParamDoc(method, "id"))
+
+  private val action = classOf[ScalaGreetingsResource].getMethod("action", classOf[java.lang.String], classOf[java.lang.Boolean], classOf[java.lang.Boolean])
+
+  private def t5 = compareDocString("<p>An action.</p>".stripMargin, provider.getMethodDoc(action))
+  private def t6 = compareDocString("<p>provides a String</p>", provider.getParamDoc(action, "param1"))
+  private def t7 = compareDocString("<p>provides a Boolean</p>", provider.getParamDoc(action, "param2"))
+
+  private def strip(string: String): String = {
+    string.replaceAll("\n", "").trim
+  }
+
+  private def compareDocString(expected: String, actual: String) = {
+    strip(actual) mustEqual strip(expected)
+  }
+}
