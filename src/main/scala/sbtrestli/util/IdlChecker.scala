95aeb7290b8563815c7825e84b6c62ddbc4d661f
Tyler Horth <tylerhorth@outlook.com>
2018-10-16 15:44:03 -0700

    Add rest model validation and publishing task

--- src/main/scala/sbtrestli/util/IdlChecker.scala
+++ src/main/scala/sbtrestli/util/IdlChecker.scala
@@ -1,42 +1,5 @@
 package sbtrestli.util
 
-import com.linkedin.restli.tools.idlcheck.{CompatibilityLevel, RestLiResourceModelCompatibilityChecker}
-import sbt.Logger
+import com.linkedin.restli.tools.idlcheck.RestLiResourceModelCompatibilityChecker
 
-object IdlChecker extends RestModelChecker {
-  override def name = "idl"
-  override def fileGlob = "*.restspec.json"
-
-  /**
-    * Checks each pair of restspec.json files (current, previous) for compatibility.  Throws an exception containing
-    * incompatibility details if incompatible changes, for the given mode, were found.
-    *
-    * @param filePairs (current, previous) file pairs
-    * @param compatLevel provides the compatibility mode to use.  Must be one of: "equivalent", "backwards", "ignore", "off"
-    * @param log provides a logger
-    * @return A error message string if ANY compatibility differences were found (even if compatMode is "ignore"),
-    *         always None if compatMode is "off"
-    */
-  override def checkCompatibility(filePairs: Seq[(java.io.File, java.io.File)], resolverPath: String,
-    compatLevel: CompatibilityLevel, log: Logger): Option[String] = {
-    val idlChecker = new RestLiResourceModelCompatibilityChecker()
-    idlChecker.setResolverPath(resolverPath)
-
-    filePairs.map { case (currentFile, previousFile) =>
-      idlChecker.check(previousFile.getAbsolutePath, currentFile.getAbsolutePath, compatLevel)
-    }
-
-    if (idlChecker.getInfoMap.isEquivalent) {
-      None
-    } else {
-      val allCheckMessage = idlChecker.getInfoMap.createSummary()
-      val allCheckMessageWithDirections = allCheckMessage + directionsMessage(compatLevel)
-
-      if (idlChecker.getInfoMap.isCompatible(compatLevel)) {
-        Some(allCheckMessageWithDirections)
-      } else {
-        throw new Exception(allCheckMessageWithDirections)
-      }
-    }
-  }
-}
+class IdlChecker extends RestLiResourceModelCompatibilityChecker with RestModelChecker
--- src/main/scala/sbtrestli/util/SnapshotChecker.scala
+++ src/main/scala/sbtrestli/util/SnapshotChecker.scala
@@ -1,50 +1,5 @@
 package sbtrestli.util
 
-import com.linkedin.restli.tools.compatibility.CompatibilityInfoMap
-import com.linkedin.restli.tools.idlcheck.CompatibilityLevel
 import com.linkedin.restli.tools.snapshot.check.RestLiSnapshotCompatibilityChecker
-import sbt.Logger
 
-object SnapshotChecker extends RestModelChecker {
-  override def name = "snapshot"
-  override def fileGlob = "*.snapshot.json"
-
-  /**
-   * Checks each pair of snapshot.json files (current, previous) for compatibility.  Throws an exception containing
-   * incompatibility details if incompatible changes, for the given mode, were found.
-   *
-   * Snapshot files contain idl and pdsc information and are ideal for perform exhaustive compatibility checking
-   *
-   * @param filePairs (current, previous) file pairs
-   * @param compatLevel provides the compatibility mode to use.  Must be one of: "equivalent", "backwards", "ignore", "off"
-   * @param log provides a logger
-   * @return A error message string if ANY compatibility differences were found (even if compatMode is "ignore"),
-   *         always None if compatMode is "off"
-   */
-  override def checkCompatibility(filePairs: Seq[(java.io.File, java.io.File)], resolverPath: String,
-                                  compatLevel: CompatibilityLevel, log: Logger): Option[String] = {
-    val snapshotChecker = new RestLiSnapshotCompatibilityChecker()
-    snapshotChecker.setResolverPath(resolverPath)
-
-    val compatibilityMap = new CompatibilityInfoMap
-
-    // check compatibility of each set of files
-    filePairs.foreach { case (currentFile, previousFile) =>
-      val infoMap = snapshotChecker.check(previousFile.getAbsolutePath, currentFile.getAbsolutePath, compatLevel)
-      compatibilityMap.addAll(infoMap)
-    }
-
-    if (compatibilityMap.isEquivalent) {
-      None
-    } else {
-      val allCheckMessage = compatibilityMap.createSummary()
-      val allCheckMessageWithDirections = allCheckMessage + directionsMessage(compatLevel)
-
-      if (compatibilityMap.isCompatible(compatLevel)) {
-        Some(allCheckMessageWithDirections)
-      } else {
-        throw new Exception(allCheckMessageWithDirections)
-      }
-    }
-  }
-}
+class SnapshotChecker extends RestLiSnapshotCompatibilityChecker with RestModelChecker
