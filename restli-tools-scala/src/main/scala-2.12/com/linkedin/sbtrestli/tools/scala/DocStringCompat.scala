3ec33b1bfbf8486948eb4dbec33f17ff5fc02287
Tyler Horth <tylerhorth@outlook.com>
2018-11-19 15:32:15 -0800

    Rename packages
    resolves #13 and #18

---
+++ restli-tools-scala/src/main/scala-2.12/com/linkedin/sbtrestli/tools/scala/DocStringCompat.scala
@@ -0,0 +1,43 @@
+package com.linkedin.sbtrestli.tools.scala
+
+import scala.tools.nsc.doc.base._
+import scala.tools.nsc.doc.base.comment._
+
+object DocStringCompat extends DocString {
+  override protected def linkToDocString: PartialFunction[LinkTo, String] = super.linkToDocString orElse {
+    case LinkToExternalTpl(name, baseUrl, _) => s"""<a href="$baseUrl">$name</a>"""
+  }
+
+  override protected def blockDocString: PartialFunction[Block, String] = super.blockDocString orElse {
+    case Table(header, columnOptions, rows) =>
+      val head = rowDocString(header, columnOptions, isHeader = true)
+      val body = rows.map(row => rowDocString(row, columnOptions)).mkString
+      s"<table><thead>$head</thead><tbody>$body</tbody></table>"
+  }
+
+  private def alignAttr(columnOption: ColumnOption): String = {
+    columnOption match {
+      case ColumnOption.ColumnOptionCenter => " align=\"center\""
+      case ColumnOption.ColumnOptionRight => " align=\"right\""
+      case _ => ""
+    }
+  }
+
+  private def rowDocString(row: Row, columnOptions: Seq[ColumnOption], isHeader: Boolean = false): String = {
+    val Row(cells) = row
+
+    val cellDocStrings = cells
+      .zip(columnOptions)
+      .map { case (cell, columnOption) =>
+        cellDocString(cell, columnOption, isHeader)
+      }
+
+    s"<tr>${cellDocStrings.mkString}</tr>"
+  }
+
+  private def cellDocString(cell: Cell, columnOption: ColumnOption, isHeader: Boolean = false): String = {
+    val Cell(blocks) = cell
+    val tag = if (isHeader) "th" else "td"
+    s"<$tag${alignAttr(columnOption)}>${blocks.map(blockDocString).mkString}</$tag>"
+  }
+}
---
+++ sbt-restli/src/main/scala/com/linkedin/sbtrestli/RestliAvroPlugin.scala
@@ -0,0 +1,68 @@
+package com.linkedin.sbtrestli
+
+import java.io.File
+
+import com.linkedin.data.avro.generator.AvroSchemaGenerator
+import org.apache.logging.log4j.{Level => XLevel}
+import sbt.Keys._
+import sbt._
+import sbt.plugins.JvmPlugin
+
+object RestliAvroPlugin extends AutoPlugin {
+  object autoImport {
+    val restliAvroGenerate = taskKey[Seq[File]]("Generates avro schemas from pegasus data-schemas.")
+    val restliAvroPackage = taskKey[File]("Packages avro schemas into *-avro-schema.jar")
+
+    val restliAvroSettings: Seq[Def.Setting[_]] = Seq(
+      includeFilter in restliAvroGenerate := "*.pdsc",
+      excludeFilter in restliAvroGenerate := HiddenFileFilter,
+
+      sourceDirectory in restliAvroGenerate := sourceDirectory.value / "pegasus",
+      sourceDirectories in restliAvroGenerate := Seq((sourceDirectory in restliAvroGenerate).value),
+
+      sources in restliAvroGenerate := Defaults.collectFiles(
+        sourceDirectories in restliAvroGenerate,
+        includeFilter in restliAvroGenerate,
+        excludeFilter in restliAvroGenerate).value,
+
+      target in restliAvroGenerate := file(sourceDirectory.value + "GeneratedAvroSchema"),
+
+      PluginCompat.watchSourcesSetting(restliAvroGenerate),
+      PluginCompat.cleanFilesSetting(restliAvroGenerate),
+
+      restliAvroGenerate := generate.value,
+
+      resourceGenerators += restliAvroGenerate.taskValue,
+      managedResourceDirectories += (target in restliAvroGenerate).value,
+      compile := compile.dependsOn(restliAvroGenerate).value,
+
+      artifactClassifier in restliAvroPackage := Some("avro-schema"),
+      publishArtifact in restliAvroPackage := true,
+
+      packagedArtifacts in Defaults.ConfigGlobal ++= Classpaths.packaged(Seq(restliAvroPackage)).value,
+      artifacts in Defaults.ConfigGlobal ++= Classpaths.artifactDefs(Seq(restliAvroPackage)).value
+    ) ++ Defaults.packageTaskSettings(restliAvroPackage, Defaults.relativeMappings(restliAvroGenerate, managedResourceDirectories))
+  }
+
+  import autoImport._
+
+  override def requires: Plugins = JvmPlugin
+
+  override def projectSettings: Seq[Def.Setting[_]] =
+    inConfig(Compile)(restliAvroSettings) ++ inConfig(Test)(restliAvroSettings)
+
+  private lazy val generate = Def.task {
+    val resolverFiles = (sourceDirectories in restliAvroGenerate).value ++
+      managedClasspath.value.files ++
+      internalDependencyClasspath.value.files
+    val resolverPath = resolverFiles.map(_.getAbsolutePath).mkString(File.pathSeparator)
+    val targetDir = (target in restliAvroGenerate).value
+    val sourceFiles = (sources in restliAvroGenerate).value.map(_.getAbsolutePath).toArray
+
+    PluginCompat.setLogLevel(classOf[AvroSchemaGenerator].getName, XLevel.INFO)
+
+    AvroSchemaGenerator.run(resolverPath, null, false, targetDir.getAbsolutePath, sourceFiles)
+
+    (targetDir ** "*.avsc").get
+  }
+}
